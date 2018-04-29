b5676ec1fb50fb15d06588c46e55b87a5397044c
Anthony Lamping <lamping.ap@gmail.com>
2018-04-29 09:31:35 -0400

    add time to look around

--- test/rostest_px4_run.sh
+++ test/rostest_px4_run.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo ${BASH_SOURCE[0]}
+echo ${DIR}
 PX4_SRC_DIR=${DIR}/..
 PX4_BIN_SIR=${DIR}/../../../bin
 source /opt/ros/kinetic/setup.bash
 source ${PX4_SRC_DIR}/Tools/setup_gazebo.bash ${PX4_SRC_DIR} ${PX4_SRC_DIR}/build/posix_sitl_default
 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:${PX4_BIN_DIR}:${PX4_SRC_DIR}:${PX4_SRC_DIR}/Tools/sitl_gazebo
+
+sleep 30m
 
 rostest px4 "$@"
