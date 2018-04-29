ac670914085d8e495ff54a4fa545a5c1944ea301
Anthony Lamping <lamping.ap@gmail.com>
2018-04-29 10:46:24 -0400

    fix dirs

--- test/rostest_px4_run.sh
+++ test/rostest_px4_run.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo ${BASH_SOURCE[0]}
-echo ${DIR}
-PX4_SRC_DIR=${DIR}/..
-PX4_BIN_SIR=${DIR}/../../../bin
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PX4_SRC_DIR="$( cd "${DIR}/../" && pwd )"
+PX4_BIN_DIR="$( cd "${DIR}/../../../bin/" && pwd )"
+
 source /opt/ros/kinetic/setup.bash
 source ${PX4_SRC_DIR}/Tools/setup_gazebo.bash ${PX4_SRC_DIR} ${PX4_SRC_DIR}/build/posix_sitl_default
 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:${PX4_BIN_DIR}:${PX4_SRC_DIR}:${PX4_SRC_DIR}/Tools/sitl_gazebo
+echo ${ROS_PACKAGE_PATH}
 
-sleep 30m
+echo "$(ls -l)"
 
 rostest px4 "$@"
